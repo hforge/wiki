285a63febd248d6d31e2b6f9315f93aea940909e
J. David Ibáñez <jdavid.ibp@gmail.com>
2017-08-23 16:12:40 +0200

    Fix install with latest itools version

--- setup.py
+++ setup.py
@@ -16,8 +16,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Import from itools
+from itools.core import get_abspath
 from itools.pkg import setup
 
 
 if __name__ == '__main__':
-    setup()
+    path = get_abspath('./')
+    setup(path)
